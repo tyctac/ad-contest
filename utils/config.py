c99ac03dbf95153a43817260a33c65f0ef53e6d2
zw <2283499971@qq.com>
2017-05-31 21:10:46 +0800

    edit config.get_home_dir

--- utils/config.py
+++ utils/config.py
@@ -4,7 +4,7 @@ import sys,os
 
 def get_home_dir(): ## 注意  在这种情况下 字符串'tianchi'只能出现在项目根目录中,否则会出错
     oristr = os.getcwd()
-    return oristr.split('tencent-ad-contest')[0] + 'tencent-ad-contest/'
+    return oristr.split('ad-contest')[0] + 'ad-contest/'
 
 def get_title_weight():
     hir = get_home_dir()
